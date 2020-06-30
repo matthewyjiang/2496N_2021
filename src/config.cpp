2261d659adf5cc98b5644a2d0c4ceac62be39925
realjohnpark <33354796+realjohnpark@users.noreply.github.com>
2020-06-30 12:12:19 -0700

    Update config.cpp

--- src/config.cpp
+++ src/config.cpp
@@ -4,5 +4,5 @@
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 pros::Motor front_left_mtr(1, pros::E_MOTOR_GEARSET_36, true);
 pros::Motor back_left_mtr(2, pros::E_MOTOR_GEARSET_36, true);
-pros::Motor front_right_mtr(2, pros::E_MOTOR_GEARSET_36);
+pros::Motor front_right_mtr(3, pros::E_MOTOR_GEARSET_36);
 pros::Motor back_right_mtr(4, pros::E_MOTOR_GEARSET_36);
