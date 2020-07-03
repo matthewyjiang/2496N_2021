dd5714d10fb171ce1c5b0b991e2edd8f53b165f9
Matthew Jiang <67533680+matthewyjiang@users.noreply.github.com>
2020-07-02 21:03:29 -0700

    Update config.h

--- include/config.h
+++ include/config.h
@@ -8,5 +8,6 @@ extern pros::Motor front_right_mtr;
 extern pros::Motor back_right_mtr;
 extern pros::Motor left_roller_mtr;
 extern pros::Motor right_roller_mtr;
+
 extern pros::ADIEncoder left_encoder;
 extern pros::ADIEncoder right_encoder;
