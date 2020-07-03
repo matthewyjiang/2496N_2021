2ab2472bd4869b52a78292dc5750bc9bb178c115
realjohnpark <johnpark1870@gmail.com>
2020-07-03 02:05:36 -0700

    im dumb

--- src/movementlib.cpp
+++ src/movementlib.cpp
@@ -7,11 +7,13 @@ void stopDrive() {
 }
 
 void drive(int distance, double maxVoltage){
+  chassisState = NORMAL;
   ChassisStraight.target = distance;
   ChassisStraight.setMaxVoltage(maxVoltage);
 }
 
 void pivot(int degree, double maxVoltage){
+  chassisState = PIVOT;
   ChassisTurn.setMaxVoltage(maxVoltage);
   double radian = (degree*acos(-1,0))/180;
   ChassisTurn.target = chassisTurn.currentPosition - radian;
